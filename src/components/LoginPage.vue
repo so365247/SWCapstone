70bc0d9e9864a888b125f45a14798a543fcef34f
antk9129@naver.com <antk9129@naver.com>
2024-11-12 00:47:44 +0900

    Design: 로그인 화면 구현

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,37 @@
+<template>
+    <div style="background-color: #2a2a2a; display: flex; justify-content: center; align-items: center; height: 100vh;">
+        <div style="padding: 20px; border-radius: 10px; max-width: 400px; width: 100%; text-align: center;">
+            <h1 style="color: #5cffd1; font-weight: bold; margin-bottom: 20px;">GUARDIAN</h1>
+    
+            <v-text-field
+                placeholder="ID"
+                color="grey"
+                style="background-color: #3a3a3a; color: white; border-radius: 5px; margin-bottom: 15px;"
+            ></v-text-field>
+    
+            <v-text-field
+                :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+                :type="visible ? 'text' : 'password'"
+                placeholder="PW"
+                color="grey"
+                style="background-color: #3a3a3a; color: white; border-radius: 5px; margin-bottom: 20px;"
+                @click:append-inner="visible = !visible"
+            ></v-text-field>
+    
+            <v-btn
+                color="#5cffd1"
+                style="height: 50px; border-radius: 25px; color: black;"
+                block
+            >
+                login
+            </v-btn>
+    
+            <div style="margin-top: 15px; color: #5cffd1;">
+                <a href="#" style="text-decoration: none; color: #5cffd1; margin-right: 10px;">아이디 찾기</a>
+                <a href="#" style="text-decoration: none; color: #5cffd1; margin-left: 10px; margin-right: 10px;">비밀번호 찾기</a>
+                <a href="#" style="text-decoration: none; color: #5cffd1; margin-left: 10px;">회원가입</a>
+            </div>
+        </div>
+    </div>
+</template>
+    
